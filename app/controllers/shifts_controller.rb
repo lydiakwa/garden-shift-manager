54c710702ddab03143a9d0e4e8dbef6390dc8fd5
Lydia Kwa <lydiakwa@gmail.com>
2024-04-29 15:03:36 -0400

    created new shifts form added new controllers create form page

---
+++ app/controllers/shifts_controller.rb
@@ -0,0 +1,31 @@
+class ShiftsController < ApplicationController
+  def index
+    # GET all
+    @shifts = Shift.all
+
+    # @user = User.first
+  end
+
+  def show
+    @shift = Shift.find(params[:id])
+  end
+
+  def new
+    # code to create an empty post and send the user
+    # to the New view for it (new.html.erb), which will have a
+    # form for creating the post
+    @shift = Shift.new
+  end
+
+  def create
+    @shift = Shift.new(title: params[:shift][:title], start_date: params[:shift][:start_date], end_date: params[:shift][:end_date])
+    if @shift.save
+      redirect_to shifts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+    # code to create a new post based on the parameters that
+    # were submitted with the form (and are now available in the
+    # params hash)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_26_204552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shifts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "comment"
+    t.datetime "start_date", null: false
+    t.datetime "end_date", null: false
+    t.integer "visitor_count", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
