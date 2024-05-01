25d70b69415bca691e00900d7db89f9aac2f57e4
Lydia Kwa <lydiakwa@gmail.com>
2024-05-01 11:15:19 -0400

    SEt up user model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_30_200955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shifts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "comment"
+    t.datetime "start_date", null: false
+    t.datetime "end_date", null: false
+    t.integer "visitor_count", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.string "telephone", null: false
+    t.integer "year_joined"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "unique_emails", unique: true
+  end
 
 end
